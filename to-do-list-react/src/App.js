99d3883ddc1408fb3d6975906ec176d2f27d8dfd
kzyGit <kezzyangiro@gmail.com>
2018-02-22 08:59:04 +0300

    Initial Commit

---
+++ to-do-list-react/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            inputItem:'',
+            ItemsList:[]
+        }
+    }
+
+    onChange = (event) => {
+        this.setState({inputItem: event.target.value});
+    } 
+
+    onSubmit = (event) => {
+      event.preventDefault();
+
+      if (this.state.inputItem === ''){return false}
+      else {
+       
+        this.setState({
+            inputItem:'',
+            ItemsList: [...this.state.ItemsList, this.state.inputItem]
+        })}
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React Todo List</h1>
+        </header>
+
+        <form onSubmit={this.onSubmit}>
+
+        
+
+        <input type = "text" 
+        className = "textInput"
+        value={this.state.inputItem}
+        onChange={this.onChange}
+        />
+
+       
+
+        <button className="btn">Submit</button>
+        </form>
+
+        <h4 className="listTitle">My Tasks List</h4>
+
+        <div className="Taskslist"> 
+        <ol>{this.state.ItemsList.map((val) => <li>{val}</li>)}</ol>
+      </div>
+
+      </div>
+
+
+    );
+  }
+}
+
+export default App;
